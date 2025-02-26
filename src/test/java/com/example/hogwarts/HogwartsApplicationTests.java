64c6747769fa1e37d52faa43c63e59a06ba3557a
Vladislav <tekuzar@mail,ru>
2025-02-26 19:52:05 +0300

    Init!

---
+++ src/test/java/com/example/hogwarts/HogwartsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.hogwarts;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
